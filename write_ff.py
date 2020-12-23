31c11b568d98ba4df900235d6e3fd265b6653e64
MatKie <mtth.kiesel@gmail.com>
2020-12-23 16:50:53 +0100

    Initial release, no functioning bonded parameters, no eq. water box

--- write_ff.py
+++ write_ff.py
@@ -1,21 +1,38 @@
 from mkutils.gromacs_writer import FFWriter
 import json, os
 
-ff_file = 'saft_parameters.json'
+ff_file = 'sdk_parameters.json'
+
+def K(kcal):
+    R = 8.3144598
+    kcal_to_J = 4186.8
+    return kcal*kcal_to_J/R
 
 if not os.path.isfile(ff_file):
     with open(ff_file, 'w') as f:
         _dict = {'atomtypes': {}, 'crossints': {}}       
         json.dump(_dict, f)
 
-ff = FFWriter(ff_file)
+ff = FFWriter(ff_file, rule='SDK')
 
-ff.add_atomtype('Wift25', 8., 6., 305.21, 0.29016, 18.015028, 10)
+ff.add_atomtype('sdkW', 12., 4., K(0.7), 0.4371, 54.045, 30)
+ff.add_atomtype('sdkSOD', 12., 4., K(0.35), 0.4371, 77.035, 41)
+ff.add_atomtype('sdkCLA', 12., 4., K(0.35), 0.4371, 71.483, 37)
+ff.add_atomtype('sdkSO4', 9., 6., K(0.7), 0.4321, 96.06 , 48)
+ff.add_atomtype('sdkCM', 9., 6., K(0.42), 0.4506, 42.07914 , 24)
+ff.add_atomtype('sdkCT', 9., 6., K(0.469), 0.4585, 43.07914 , 25)
 
-ff.add_atomtype('CT', 15.947, 6., 358.37, 0.45012, 43.08698, 25)
 
+ff.add_crossint('sdkW', 'sdkSOD', eps_mix=K(0.895))
+ff.add_crossint('sdkW', 'sdkCLA', eps_mix=K(0.895))
+ff.add_crossint('sdkW', 'sdkSO4', eps_mix=K(1.1))
+ff.add_crossint('sdkW', 'sdkCM', eps_mix=K(0.34))
+ff.add_crossint('sdkW', 'sdkCT', eps_mix=K(0.36))
 
-ff.add_crossint('Wift25', 'CT', k=0.31)
+ff.add_crossint('sdkSOD', 'sdkCLA', eps_mix=K(0.895))
+ff.add_crossint('sdkSOD', 'sdkSO4', eps_mix=K(1.1))
+ff.add_crossint('sdkSOD', 'sdkCM', eps_mix=K(0.895))
+ff.add_crossint('sdkSOD', 'sdkCT', eps_mix=K(0.895))
 
 ff.write_tables(shift=True, cutoff=1.5)
 ff.write_forcefield(outfile='ffnonbonded.itp')
